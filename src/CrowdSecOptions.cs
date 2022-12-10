d109e31e6c0c7583fc8c891d5828342b3bb269bf
Thomas <skovgaard.andersen@gmail.com>
2022-12-10 15:08:04 +0100

    removed useless options comments

--- src/CrowdSecOptions.cs
+++ src/CrowdSecOptions.cs
@@ -2,21 +2,8 @@ namespace AspNetCore.CrowdSec;
 
 public class CrowdSecOptions
 {
-    /// <summary>
-    /// The CrowdSec Local API key.
-    /// </summary>
     public string ApiKey { get; set; } = string.Empty;
-    
-    /// <summary>
-    /// The URI for the CrowdSec API server. The default is http://localhost
-    /// </summary>
     public Uri ApiServerUrl { get; set; } = new("http://localhost");
-    
-    /// <summary>
-    /// CrowdSec API request timeout. The default is 10 minutes.
-    /// </summary>
     public TimeSpan ApiServerRequestTimeout { get; set; } = TimeSpan.FromMinutes(10);
-
-    // TODO (kaetsjup): this needs refactoring.
     public Uri CaptchaChallengeUrl { get; set; } = new("/captcha.html");
 }
--- src/IpAddressQueryResult.cs
+++ src/IpAddressQueryResult.cs
@@ -6,8 +6,7 @@ public class IpAddressQueryResult
     public static readonly IpAddressQueryResult Unknown = new(1, "ok");
     public static readonly IpAddressQueryResult Banned = new(2, "ban");
     public static readonly IpAddressQueryResult Challenge = new(3, "challenge");
-
-
+    
     private IpAddressQueryResult(int id, string name) 
         : base(id, name)
     {
